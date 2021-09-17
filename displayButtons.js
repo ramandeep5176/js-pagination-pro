dbc7fad36ebfbf6c22aa29d953da967df5bd31e8
ramandeep5176 <ramandeepgill5176@gmail.com>
2021-09-16 21:10:54 -0400

    completed

--- displayButtons.js
+++ displayButtons.js
@@ -1,3 +1,18 @@
-const displayButtons = () => {}
+const displayButtons = (container, pages, activeIndex) => {
+  let btns = pages.map((_, pagesIndex) => {
+    return `
+    <button class="page-btn ${
+      activeIndex === pagesIndex ? "active-btn" : "null"
+    }" data-index="${pagesIndex}">
+    ${pagesIndex + 1}
+    </button>`;
+  });
+  // prev-next
+  btns.push(`<button class="next-btn">next</button>`);
 
-export default displayButtons
+  btns.unshift(`<button class="prev-btn">prev</button>`);
+
+  container.innerHTML = btns.join("");
+};
+
+export default displayButtons;
--- paginate.js
+++ paginate.js
@@ -1,3 +1,13 @@
-const paginate = () => {}
+//
 
-export default paginate
+const paginate = (followers) => {
+  const itemPerPage = 10;
+  const numberOfPages = Math.ceil(followers.length / itemPerPage);
+  const newFollowers = Array.from({ length: numberOfPages }, (_, index) => {
+    const start = index * itemPerPage;
+    return followers.slice(start, start + itemPerPage);
+  });
+  return newFollowers;
+};
+
+export default paginate;
